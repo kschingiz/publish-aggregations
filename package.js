89fbbe72214dc15c693fe5299380d533c996a233
Shynggys <kschingiz@gmail.com>
2018-08-15 11:24:18 +0600

    initial commit

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'kschingiz:publish-aggregations',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.7.0.3');
+  api.use('ecmascript');
+  api.mainModule('publish-aggregations.js');
+});
+
+Package.onTest(function (api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('publish-aggregations');
+  api.mainModule('publish-aggregations-tests.js');
+});
---
+++ publish-aggregations-tests.js
@@ -0,0 +1,11 @@
+// Import Tinytest from the tinytest Meteor package.
+import { Tinytest } from "meteor/tinytest";
+
+// Import and rename a variable exported by publish-aggregations.js.
+import { name as packageName } from "meteor/publish-aggregations";
+
+// Write your tests here!
+// Here is an example.
+Tinytest.add('publish-aggregations - example', function (test) {
+  test.equal(packageName, "publish-aggregations");
+});
