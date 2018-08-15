4e715715f213661c74ec48f54a7af51e2ccc64cb
Shynggys <kschingiz@gmail.com>
2018-08-15 11:39:51 +0600

    package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'kschingiz:publish-aggregations',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'This package lets to publish aggregation with pipeline and options in MeteorJS',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/kschingiz/publish-aggregations',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
